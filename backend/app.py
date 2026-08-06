b4d7af3c58ba4e4b153c89d9433bb3a77c15d525
Kanishka Gupta <kanishkagupta0402@gmail.com>
2026-08-06 19:15:31 +0530

    Fix production CORS

--- backend/app.py
+++ backend/app.py
@@ -21,10 +21,8 @@ app.mount(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-        "http://localhost:5174",
-        "http://localhost:5175"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
